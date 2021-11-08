9585e0f4fc045fd78fb3020f7488ebf170ad74d4
Riship Gupta <rishipgupta92@gmail.com>
2021-11-08 14:54:15 +0530

    use variable to store JSX content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,6 +13,21 @@ const Expenses = ({ expenses }) => {
   const filteredExpenses = expenses.filter(
     (expense) => expense.date.getFullYear().toString() === filteredYear
   );
+
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length !== 0) {
+    expensesContent = filteredExpenses.map((expense) => {
+      // console.log(expense);
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    });
+  }
   return (
     <div>
       <Card className='expenses'>
@@ -20,32 +35,7 @@ const Expenses = ({ expenses }) => {
           selected={filteredYear}
           onFilterByYear={filterExpensesByYear}
         />
-        {filteredExpenses.length > 0 ? (
-          filteredExpenses.map((expense) => {
-            // console.log(expense);
-            return (
-              <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-              />
-            );
-          })
-        ) : (
-          <p>No expenses found.</p>
-        )}
-        {/* {filteredExpenses.map((expense) => {
-          // console.log(expense);
-          return (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          );
-        })} */}
+        {expensesContent}
       </Card>
     </div>
   );
